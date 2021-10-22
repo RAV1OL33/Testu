a899df8e1d2517ef4d0194de741c254b8e3b5c98
RAV1OL33 <mr.expel@mail.ru>
2021-10-22 10:46:46 +0300

    First approach for project skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import {AppRoutingModule, ArrayOfComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
 import { ItemsModule } from "./items/items.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, } from "@angular/material/button";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+//TODO putt all material modules in diff file
 
 @NgModule({
   declarations: [
@@ -16,6 +24,13 @@ import { ItemsModule } from "./items/items.module";
     AppRoutingModule,
     SharedModule,
     ItemsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
