7cf85e9173e5d7329c3366cf92bfb58df952ce94
RAV1OL33 <mr.expel@mail.ru>
2021-10-25 14:03:10 +0300

    @angular/material deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,6 @@ import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
 import { ItemsModule } from "./items/items.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, } from "@angular/material/button";
-import {MatSliderModule} from "@angular/material/slider";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
 //TODO putt all material modules in diff file
 
 @NgModule({
@@ -25,12 +19,6 @@ import {MatInputModule} from "@angular/material/input";
     SharedModule,
     ItemsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatSliderModule,
-    MatExpansionModule,
-    MatTabsModule,
-    MatFormFieldModule,
-    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
